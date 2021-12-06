c06a8a891e6e3d7a0e6972914992638e4c13d057
sassmander <91296660+sassmander@users.noreply.github.com>
2021-12-06 03:31:22 -0500

    Ongoing audio files
    For Fariza

---
+++ Audio_SFML/audio.cpp
@@ -0,0 +1,64 @@
+
+#include "audio.h"
+
+using namespace sf;
+
+void stoneclatter(){
+
+}
+    void wingame(){
+
+    }
+    void losegame(){
+
+    }
+    void backgroundmusic(){
+        
+    }
+
+
+{
+    RenderWindow game(VideoMode(1007, 1007), "Mancala ver EC327");
+
+    Texture title, win, lose;
+
+    title.loadFromFile("images/Title.PNG");
+    // win.loadFromFile();
+    // lose.loadFromFile();
+
+    SoundBuffer buffer;
+
+    Music music;
+
+    if (!buffer.loadFromFile("images/clatter.wav"))
+      exit(0); // error
+    
+    Sprite sBackground1(title);
+    Sound sound;
+    sound.setBuffer(buffer);
+    sound.play();
+
+    //CircleShape shape(500.f);
+    //shape.setFillColor(Color::Blue);
+
+    while (game.isOpen())
+    {
+        Event event;
+        while (game.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                game.close();
+        }
+
+        game.clear();
+        game.draw(sBackground1);
+        game.display();
+    }
+//Fariza
+/*RenderWindow window(VideoMode(640,480),"Mancala EC327");
+sf::SoundBuffer buffer;
+if(!buffer.loadFromFile)*/
+
+
+    return 0;
+}
